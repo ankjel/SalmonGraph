c920c32ac723ced147607cfd2a27106b21122535
ankjelst <anna.s.kjelstrup@gmail.com>
2022-01-11 19:35:42 +0100

    adjusted script for cram

--- scripts/prdm9/collect_reads.sh
+++ scripts/prdm9/collect_reads.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-#SBATCH --ntasks=8
+#SBATCH --ntasks=1
 #SBATCH --nodes=1                # Use 1 node
-#SBATCH --job-name=pggb  # sensible name for the job
-#SBATCH --mem=30G                 # Default memory per CPU is 3GB.
-#SBATCH --output=log-pggb-%j.out
+#SBATCH --job-name=samtools  # sensible name for the job
+#SBATCH --mem=3G                 # Default memory per CPU is 3GB.
+#SBATCH --output=log-samtools-%j.out
 
 
 
-# This script takes a cram file as input and collects reads from a given region. 
-
-cram=$1
+# This script takes one ore several cram files and collects reads from a given region. 
 
 module load SAMtools/1.11-GCC-9.3.0
-mkdir prdm9_both_haps
 
-for bam in Alto.bam Arnold.bam Barry.bam Bond.bam Brian.bam Klopp.bam Louis.bam Maxine.bam Simon.bam Tanner.bam Tess.bam
+
+cd $SCRATCH/data/prdm9
+mkdir -p prdm9_both_haps
+
+for bam in tess.cram
 do
         input=$bam
         region1='ssa05:12773188-127773343' # This depends on the reference
-        ind=${bam/.bam/}
+        ind=$(basename $input)
 
-        samtools view -H $input > header.sam
+        samtools view -H $input > header.sam # Extract the header to merge with reads later for valid bam
+        # First: subset region, second: cat header and region for valid sam, 
+        #third: S ignore compability something abot samtools version, b bam output 
         samtools view $input "$region1" | cat header.sam - | samtools view -Sb - > ${ind}_${region1}.bam
+        # index new bam file
         samtools index ${ind}_${region1}.bam
+        # make fasta from new bam file
         samtools fasta ${ind}_${region1}.bam > prdm9_both_haps/${ind}_${region1}_reads.fa
-
+        
         rm ${ind}_${region1}.bam*
 
-done4
-
-
+done
 
