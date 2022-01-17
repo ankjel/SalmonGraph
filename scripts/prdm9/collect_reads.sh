25a7e279ba92a8610927c2af65620e9724ffe96e
ankjelst <anna.s.kjelstrup@gmail.com>
2022-01-17 18:04:22 +0100

    Improved script for fishing out reads

--- scripts/prdm9/collect_reads.sh
+++ scripts/prdm9/collect_reads.sh
@@ -10,7 +10,8 @@
 
 # This script takes one ore several cram files and collects reads from a given region. 
 
-module load SAMtools/1.11-GCC-9.3.0
+#module load SAMtools/1.11-GCC-9.3.0
+# samtools in modules is way too old
 
 
 cd $SCRATCH/data/prdm9
@@ -19,29 +20,51 @@ mkdir -p prdm9_both_haps
 for bam in tess.cram
 do
         input=$bam
-        region1='ssa05:12773188-127773343' # This depends on the reference
+        region1='ssa05:12773150-12773892' # This depends on the reference- 
+        # Øyvind 12773150-12773892 Kristina 12773188-127773343 for simon
         ind=$(basename $input)
-    # https://www.biostars.org/p/68358/#68362
+        
+        #############################################
+        # Find all the reads mapping to our region
+        #############################################
+        
+        singularity exec /cvmfs/singularity.galaxyproject.org/s/a/samtools:1.14--hb421002_0 \
         samtools view -@ $SLURM_CPUS_ON_NODE -H $input > header.sam # Extract the header to merge with reads later for valid bam
         # First: subset region, second: cat header and region for valid sam, 
         #third: S ignore compability something abot samtools version, b bam output 
-        samtools view -@ $SLURM_CPUS_ON_NODE $input -F 4 "$region1" | cat header.sam - | \ # -F 4 exclude unmapped reads
+        
+        singularity exec /cvmfs/singularity.galaxyproject.org/s/a/samtools:1.14--hb421002_0 \
+        samtools view -@ $SLURM_CPUS_ON_NODE $input -F 4 "$region1" | cat header.sam - | singularity exec \
+        /cvmfs/singularity.galaxyproject.org/s/a/samtools:1.14--hb421002_0 \
         samtools view -@ $SLURM_CPUS_ON_NODE -Sb - > ${ind}_${region1}.bam
         
         # index new bam file
+        singularity exec /cvmfs/singularity.galaxyproject.org/s/a/samtools:1.14--hb421002_0 \
         samtools index -@ $SLURM_CPUS_ON_NODE ${ind}_${region1}.bam
         
-        # make fasta from new bam file
-        samtools fastq -@ $SLURM_CPUS_ON_NODE ${ind}_${region1}.bam > prdm9_both_haps/${ind}_${region1}_reads.fq
         
-        # Sort reads into two files for giraffe.
-        cat prdm9_both_haps/${ind}_${region1}_reads.fq | \
-        grep "^@.*/1$" -A 3 --no-group-separator > prdm9_both_haps/${ind}_${region1}_r1.fq
+        #########################################################
+        # Get all the pairs where one maps to region
+        ########################################################
         
-        cat prdm9_both_haps/${ind}_${region1}_reads.fq | \
-        grep "^@.*/2$" -A 3 --no-group-separator > prdm9_both_haps/${ind}_${region1}_r2.fq
+        #Find names of all reads in region
+        samtools view ${ind}_${region1}.bam | awk '{print $1}' > names.txt
         
-        rm ${ind}_${region1}.bam* 
+        #extract all reads mapped and in pairs with one of these names, add heades
+        singularity exec /cvmfs/singularity.galaxyproject.org/s/a/samtools:1.14--hb421002_0 \
+        samtools view -@ $SLURM_CPUS_ON_NODE -F 260 -N names.txt $bam | cat header.sam - | singularity exec \
+        /cvmfs/singularity.galaxyproject.org/s/a/samtools:1.14--hb421002_0 \
+        samtools view -@ $SLURM_CPUS_ON_NODE -Sb - > ${ind}_${region1}_all.bam
+        
+        # Index bam
+        singularity exec /cvmfs/singularity.galaxyproject.org/s/a/samtools:1.14--hb421002_0 \
+        samtools index -@ $SLURM_CPUS_ON_NODE ${ind}_${region1}_all.bam   
+        
+        # Make bam into fastqs
+        singularity exec /cvmfs/singularity.galaxyproject.org/s/a/samtools:1.14--hb421002_0 \
+        samtools fastq -@ $SLURM_CPUS_ON_NODE -1 ${ind}_${region1}_all_R1.fq -2 ${ind}_${region1}_all_R2.fq -n ${ind}_${region1}_all.bam
+        
+        rm ${ind}_${region1}.bam* names.txt header.sam ${ind}_${region1}_all.bam*
 
 done
 
