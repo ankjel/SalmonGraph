d52f54ea7c2c614b35fcb21fd791e6feb741d2a9
ankjelst <anna.s.kjelstrup@gmail.com>
2022-01-20 14:12:38 +0100

    Edit script name after running order.

--- scripts/prdm9/01_pggb_construct_graph.sh
+++ scripts/prdm9/01_pggb_construct_graph.sh
@@ -3,16 +3,18 @@
 #SBATCH --ntasks=8
 #SBATCH --nodes=1                # Use 1 node
 #SBATCH --job-name=pggb  # sensible name for the job
-#SBATCH --mem=30G                 # Default memory per CPU is 3GB.
+#SBATCH --mem=3G                 # Default memory per CPU is 3GB.
 #SBATCH --output=log-pggb-%j.out
-
 #SBATCH --constraint="avx2" # IMPOTATANT!!! PGGB jobs will fail without this
 
-#INPUT : full path to one fasta file
+
 
 echo "START"
 date
 
+#INPUT : full path to one fasta file
+# NB! if you include more graphics with for instance --multiqc, --layout and --stats you should ask for more memory 
+# NB2! if you are running this script with a full chromosome input you should ask for a lot more memory
 
 
 ######
@@ -21,20 +23,20 @@ date
 fasta=$1
 
 #wfmash
-param_s=100
-param_p=95
-param_n=17  #Ideally, you should set this to equal the number of haplotypes in the pangenome.
-param_K=16
-param_i="$(basename $fasta)"
-param_l=10
+param_s=100 # segment size, this should only be this small because we have a small graph, for full chromosomes set to 100000
+param_p=95 # percent identity in the wfmash step, including variants. This should not be so strict for this small example
+param_n=13  #Ideally, you should set this to equal the number of haplotypes in the pangenome.
+param_K=16 # Kmer size for aligning
+param_i="$(basename $fasta)" 
+param_l=300 # minimum block length filter for mapping. (segments are merged to blocks, default 3*segment-length)
 
 
 #seqwish
-param_k=85
+param_k=84 #filter exact matches below this length [default: 29]
 
 #smoothxg
-param_H=17
-param_G=20000
+param_H=13 # number of haplotypes, if different than that set with -n
+param_G=20000 # target sequence length for POA, first pass = N, second pass = M [default: 13117,13219]
 
 
 #deconstruct
