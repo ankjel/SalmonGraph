511f2bfc71822f846a7cfd1daa9f79467f1ace75
ankjelst <anna.s.kjelstrup@gmail.com>
2022-05-05 19:04:58 +0200

    Final version for odgi viz figure

--- scripts/prdm9/odgiviz.sh
+++ scripts/prdm9/odgiviz.sh
@@ -14,4 +14,4 @@ graph="$SCRATCH"/data/prdm9/pggb-final-k311-p98.out/prdm9-znf.fasta-chop.gfa
 path_viz=/mnt/users/ankjelst/MasterScripts/scripts/prdm9/paths_viz.txt
 #singularity exec /mnt/users/ankjelst/tools/pggb_v0.2.0.sif odgi sort -i "$graph" -o sorted.og -O
 #singularity exec /mnt/users/ankjelst/tools/pggb_v0.2.0.sif odgi paths -i sorted.og -L
-singularity exec /mnt/users/ankjelst/tools/pggb_v0.2.0.sif odgi viz -i $graph -o zf.png -x 500 -s"#" -r "SimonResolved#2#znfArray:1-10671" -p "$path_viz"
+singularity exec /mnt/users/ankjelst/tools/pggb_v0.2.0.sif odgi viz -i $graph -o zf.png -x 500 -s"#" -r "SimonResolved#2#znfArray:5552-5672" -p "$path_viz"
